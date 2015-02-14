3308277fac0c3b9845ff517ccecf7428b12a2502
Dominic <dominicong94@gmail.com>
2015-02-13 23:54:14 -0500

    Added parse

--- CookieDelivery/AppDelegate.swift
+++ CookieDelivery/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        Parse.enableLocalDatastore()
+        Parse.setApplicationId("UNWsigwEetWXK5hZhSJVo305HdJMWyjcS0VXRORV", clientKey: "Li78zWznDmRz8HpOB5X0ocX2x8X6u6cDtGwVrAat")
+        PFAnalytics .trackAppOpenedWithLaunchOptionsInBackground(launchOptions, block: nil)
         return true
     }
 
